0270087e32a2dc96c36e580ae2b68649ab7b5b5e
Claire <78150327+zhanqifan@users.noreply.github.com>
2025-03-05 22:36:35 +0800

    Update default.js

--- aigc-server/config/default.js
+++ aigc-server/config/default.js
@@ -7,21 +7,21 @@ const dbConfig={
     port:3306
 }
 const weixin ={
-    appid:'wx3ee6184958a78513',//小程序appid
-    secret:'bd0deb76f51043fd3e56dc0b38c0bfd9',//小程序密钥
+    appid:'',//小程序appid
+    secret:'',//小程序密钥
 }
 // 智谱清言
 const zhipu={
-    apiKey:'ba327cfbea9af50f529e4dbd3a684e7f.yIuRcA4XI4R1w8LU',
+    apiKey:'',
 }
 const userToken={
     secretkey: "key",
     expiresIn: "2d",
   }
-// 语音识别密钥
+// 阿里语音识别密钥
 const Aliyunaudio={
-    accessKeyId:'LTAI5tAvunE3RBmQLqAstWW4',
-    accessKeySecret:'FqHpXIT21dkbENwkjuuYXUiHw6lvPk',
+    accessKeyId:'',
+    accessKeySecret:'',
 }
 
 module.exports={
